cb856848e406b2e998c4566dd9642fb6f4b1bc40
Mohityadav-code <mohityadav3552@gmail.com>
2023-05-03 12:59:52 +0530

    optimized code

--- Number line Jumps.js
+++ Number line Jumps.js
@@ -1,26 +1,38 @@
-var x1 = 0;
-var v1 = 3;
-var x2 = 4;
-var v2 = 2;
+function kangaroo(x1, v1, x2, v2) {
+  let t = 0;
+  while (!((x1 > x2 && v1 >= v2) || (x2 > x1 && v2 >= v1))) {
+    //condition here is if any of kangaroo has reached at the point where other kangaroo can never reach there
+    if (x1 + v1 * t === x2 + v2 * t) {
+      // here cheking if they are at same point
+      return "YES";
+    }
+    t++;
+  }
+  return "NO";
+}
+console.log(" kangaroo(0,2,5,3): ", kangaroo(0, 2, 5, 3));
+console.log(" kangaroo(0,3,4,2): ", kangaroo(2, 1, 1, 2));
 
 function kangaroo(x1, v1, x2, v2) {
-    let t = 0; // Initialize the number of jumps
-  
-    // Iterate while neither kangaroo has reached a position where the other can no longer catch up.
-    while (!((x1 > x2 && v1 >= v2) || (x2 > x1 && v2 >= v1))) {
-      // Check if the kangaroos are at the same position after 't' jumps.
-      if (x1 + v1 * t === x2 + v2 * t) {
-        return "YES";
-      }
-  
-      // Increment the number of jumps.
-      t++;
-    }
-  
-    // If the loop exits, that means one kangaroo has reached a position where the other can no longer catch up.
+  // edge cases
+  if (v1 <= v2 && x1 < x2) {
     return "NO";
   }
-  kangaroo(0,2,5,3)
-  console.log(' kangaroo(0,2,5,3): ',  kangaroo(0,2,5,3));
-  console.log(' kangaroo(0,3,4,2): ',  kangaroo(2,1,1,2));
-  
+  if (v2 <= v1 && x2 < x1) {
+    return "NO";
+  }
+  if (v1 == v2) {
+    if (x1 == x2) {
+      return "YES";
+    } else {
+      return "NO";
+    }
+  }
+  const distance = Math.abs(x1 - x2);
+  const relativeSpeed = Math.abs(v1 - v2);
+  if (distance % relativeSpeed === 0) {
+    return "YES";
+  } else {
+    return "NO";
+  }
+}
