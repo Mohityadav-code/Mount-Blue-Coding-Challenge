46ba22499350d959efd14ee0e55b1ca1824f9736
Mohityadav-code <mohityadav3552@gmail.com>
2023-05-03 10:56:53 +0530

    update with Map method

--- Pair of shocks.js
+++ Pair of shocks.js
@@ -1,7 +1,6 @@
 var n = 7;
-var arr = [1, 2, 1, 2, 1, 3, 4, 5];
+var ar = [1, 2, 1, 2, 1, 3, 2];
 var pair = [];
-
 function ch(arr, n) {
   for (let i = 0; i < n; i++) {
     for (let j = i+1; j < n; j++) {
@@ -14,6 +13,40 @@ function ch(arr, n) {
     }
   }
 }
-ch(arr, n)
-console.log(pair);
+// ch(arr, n)
+// console.log(pair);
+// let countMap = new Map();
+// var arr=[1,2,1,2,1,3,2]
+// arr.forEach((value)=>{
+//     // check if the value is already in the map
+//     if(countMap.has(value)){
+//         countMap.set(value,countMap.get(value)+1);
+//     }
+//     else {
+//         countMap.set(value,1)
+//     }
+// })
+// console.log(countMap);
+// var pair=0
+// countMap.forEach((elements)=>{
+//     console.log(Math.floor(elements/2));
+//     pair+=Math.floor(elements/2)
+// })
+// console.log(pair);
 
+
+    var pair=0
+    let countMap=new Map();
+    ar.forEach((Element)=>{
+        if(countMap.has(Element)){
+            countMap.set(Element,countMap.get(Element)+1)
+        }
+        else{
+            countMap.set(Element,1)
+        }
+    })
+    countMap.forEach((Element)=>{
+       var temp=(Math.floor(Element/2));
+        pair+=temp
+    })
+    console.log(pair);
